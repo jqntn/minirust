772470b5d60c78dd047d78b9c02f0390b04ef97a
Julien QUENTIN <jqntn88@gmail.com>
2024-01-19 01:25:07 +0100

    try no_std + no_main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+extern "C" fn main() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
