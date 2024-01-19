fab68b65d130e48d252f29be423fe03c6be9ee0e
Julien QUENTIN <jqntn88@gmail.com>
2024-01-19 01:29:18 +0100

    revert to using std

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-#![no_std]
-#![no_main]
-
-#[no_mangle]
-extern "C" fn main() -> ! {
-    loop {}
-}
-
-#[panic_handler]
-fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn main() {
+    println!("Hello, world!");
 }
